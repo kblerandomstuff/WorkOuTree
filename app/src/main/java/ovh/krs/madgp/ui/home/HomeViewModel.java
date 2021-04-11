775218ada7dfbd12f87e1e5bd3a9a0fdcd24e0f5
kble <kble326@gmail.com>
2021-04-11 14:39:09 +0800

    layout bugged 1

---
+++ app/src/main/java/ovh/krs/madgp/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package ovh.krs.madgp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("@strings/c_intro");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
